33b30ee79dcf33fc237b886a6c11ce660baf9355
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-09 16:00:44 -0300

    feat: reafactor

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
+import { GenreResponseProps, MovieProps } from "../App"
+import { MovieCard } from "./MovieCard"
+import "../styles/content.scss";
+
+type ContetProps = {
+  movies: MovieProps[],
+  selectedGenre: GenreResponseProps
+}
+
+export function Content({ movies, selectedGenre }: ContetProps) {
   // Complete aqui
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+import "../styles/sidebar.scss";
+
+type SideBarProps = {
+  genres: GenreResponseProps[]
+  handleClickButton: (id: number) => void,
+  selectedGenreId: number
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: SideBarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
